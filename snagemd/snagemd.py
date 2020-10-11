1dead7ac11f6237c8593fd3e976a6cc7bbc11bb6
jcmdln <jcmdln@gmail.com>
2020-10-11 16:49:45 -0400

    snagemd: initial api scaffolding

--- snagemd/snagemd.py
+++ snagemd/snagemd.py
@@ -1,5 +1,12 @@
+import click
+import uvicorn
+
+from snagemd import api
+
+
+@click.command()
 def main() -> None:
-    pass
+    uvicorn.run(app=api.app, host="127.0.0.1", port=5050, log_level="info")
 
 
 if __name__ == "__main__":
