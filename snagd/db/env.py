273cfdce2f7816e211c78d9ac2ea9c660a09c9b6
jcmdln <jcmdln@gmail.com>
2022-03-06 14:33:29 -0500

    snagd: Combine db and alembic namespaces

--- snagd/db/env.py
+++ snagd/db/env.py
@@ -11,7 +11,8 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)  # type: ignore
+if config.config_file_name:
+    fileConfig(fname=config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -23,14 +24,10 @@ target_metadata = None
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    This configures the context with just a URL and not an Engine, though an Engine is acceptable
+    here as well. By skipping the Engine creation we don't even need a DBAPI to be available.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    Calls to context.execute() here emit the given string to the script output.
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -47,9 +44,7 @@ def run_migrations_offline() -> None:
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    In this scenario we need to create an Engine and associate a connection with the context.
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
