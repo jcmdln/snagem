2654095f928636cbbca47b2e7325f53c55b16c2e
jcmdln <jcmdln@gmail.com>
2022-03-06 15:26:46 -0500

    api: Flatten endpoints dir into parent v1 dir

--- snagd/api/v1/__init__.py
+++ snagd/api/v1/__init__.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 
 from fastapi import APIRouter
 
-from snagd.api.v1.endpoints import router as endpoints_router
+from snagd.api.v1.media import router as media_router
 
 router = APIRouter()
-router.include_router(endpoints_router)
+router.include_router(media_router)
 
 
 __all__: list[str] = ["router"]
