abf650038a427ba0359812fdb0f36afd1c89608b
jcmdln <jcmdln@gmail.com>
2022-03-06 15:13:16 -0500

    Use correct format for sqlalchemy tables

--- snagd/db/model.py
+++ snagd/db/model.py
@@ -6,14 +6,19 @@ from snagd.db import session
 
 
 class Media(session.Base):
-    categories: Column[String]
-    date_created: Column[DateTime]
-    date_updated: Column[DateTime]
-    description: Column[String]
-    duration: Column[Integer]
-    source_url: Column[String]
-    subtitles: Column[String]
-    tags: Column[String]
-    title: Column[String]
-    uuid: Column[String]
-    views: Column[Integer]
+    __tablename__ = "media"
+
+    categories = Column(String)
+    date_created = Column(DateTime)
+    date_updated = Column(DateTime)
+    description = Column(String)
+    duration = Column(Integer)
+    source_url = Column(String)
+    subtitles = Column(String)
+    tags = Column(String)
+    title = Column(String)
+    uuid = Column(String, primary_key=True)
+    views = Column(Integer)
+
+
+__all__: list[str] = ["Media"]
