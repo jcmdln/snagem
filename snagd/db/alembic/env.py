a98f25100d32f40191175952d052ed32b0210a8d
jcmdln <jcmdln@gmail.com>
2022-03-07 23:24:53 -0500

    Wire up alembic

---
+++ snagd/db/alembic/env.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# https://alembic.sqlalchemy.org/en/latest/tutorial.html
+
+from __future__ import annotations
+
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from snagd.db import session
+
+config = context.config
+metadata = session.Base.metadata
+
+if config.config_file_name:
+    fileConfig(fname=config.config_file_name)
+
+
+def run_migrations_offline() -> None:
+    url = config.get_main_option("sqlalchemy.url")
+    opts = {"paramstyle": "named"}
+    context.configure(url=url, target_metadata=metadata, literal_binds=True, dialect_opts=opts)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    cfg = config.get_section(config.config_ini_section)
+    cfg["sqlalchemy.url"] = session.db_url  # type: ignore
+    connectable = engine_from_config(cfg, prefix="sqlalchemy.", poolclass=pool.NullPool)
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
