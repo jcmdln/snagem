d9d265098003c0d092fd84ecc91bdfd86f5ff4ff
jcmdln <jcmdln@gmail.com>
2022-03-15 18:47:55 -0400

    Handle required parameters in our SQL model(s)

--- snagd/db/model.py
+++ snagd/db/model.py
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import uuid
+
+from datetime import datetime
+
 from sqlalchemy import Column, DateTime, Integer, String
 
 from snagd.db import session
@@ -8,17 +12,17 @@ from snagd.db import session
 class Media(session.Base):
     __tablename__ = "media"
 
-    uuid = Column(String, nullable=False, primary_key=True)
+    uuid = Column(String, default=str(uuid.uuid4()), nullable=False, primary_key=True, unique=True)
     categories = Column(String)
-    date_created = Column(DateTime, nullable=False)
-    date_updated = Column(DateTime, nullable=False)
+    date_created = Column(DateTime, default=datetime.now, nullable=False)
+    date_updated = Column(DateTime, default=datetime.now, nullable=False)
     description = Column(String)
-    duration = Column(Integer, nullable=False)
+    duration = Column(Integer, default=0, nullable=False)
     source_url = Column(String, nullable=False)
     subtitles = Column(String)
     tags = Column(String)
-    title = Column(String, nullable=False)
-    views = Column(Integer, nullable=False)
+    title = Column(String, default="title", nullable=False)
+    views = Column(Integer, default=0, nullable=False)
 
 
 __all__: list[str] = ["Media"]
