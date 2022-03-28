15fe333dd6632444f741e8a927d7678c7ecf8528
jcmdln <jcmdln@gmail.com>
2022-03-27 23:31:15 -0400

    Move task worker logic to its own file

--- snagd/task/__init__.py
+++ snagd/task/__init__.py
@@ -2,14 +2,6 @@
 
 from __future__ import annotations
 
-from os import getenv
+from snagd.task import media, worker
 
-from celery import Celery
-
-from snagd.task import media
-
-backend_url: str | None = getenv("BACKEND_URL")
-broker_url: str = getenv("BROKER_URL", "ampq://guest@localhost/")
-celery = Celery("snager", backend=backend_url, broker=broker_url)
-
-__all__: list[str] = ["celery", "media"]
+__all__: list[str] = ["celery", "media", "worker"]
